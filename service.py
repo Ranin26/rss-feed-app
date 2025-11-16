58cf5e11e2529c31b6e5293a926405e223393f78
Ranin26 <31333314+Ranin26@users.noreply.github.com>
2025-11-16 17:49:06 -0500

    switching to sqlite service

--- service.py
+++ service.py
@@ -17,7 +17,7 @@ PORT = int(os.getenv("PORT", 8000))
 SQLITE_SERVICE_PATH = os.getenv('SQLITE_PRIVATE_PATH', None)
 VOLUME_NAME = os.getenv("RAILWAY_VOLUME_NAME", "no volume name")
 MOUNT_PATH = SQLITE_SERVICE_PATH if SQLITE_SERVICE_PATH else os.getenv("RAILWAY_VOLUME_MOUNT_PATH", os.getenv("MOUNT_PATH","./data"))
-DB_FILE = f"{MOUNT_PATH}/feeds.db"
+DB_FILE = f"{MOUNT_PATH}/feeds" if SQLITE_SERVICE_PATH else f"{MOUNT_PATH}/feeds.db"
 DEBUG = os.getenv("DEBUG", "0").lower() in ("1", "true", "yes")  or (hasattr(sys, "gettrace") and sys.gettrace() is not None)
 
 app = FastAPI(title="RSS Feed Service")
